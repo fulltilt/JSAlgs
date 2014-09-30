2bf6e14731a0eb1d950d588524f6ac8da462f892
Dave D <dave.doria@gmail.com>
2014-09-29 22:42:12 -0700

    added bubble, selection and insertion sort

--- Sorting.js
+++ Sorting.js
@@ -5,11 +5,81 @@ function Sorting(numElements) {
   this.insert = insert;
   this.toString = toString;
   this.clear = clear;
+  this.print = print;
   this.setData = setData;
   this.swap = swap;
   
   for (var i = 0; i < numElements; ++i) {
     this.dataStore[i] = i;
+  }
+
+  this.bubbleSort = bubbleSort;
+  this.selectionSort = selectionSort;
+  this.insertionSort = insertionSort;
+}
+
+function setData() {
+  for (var i = 0; i < this.numElements; i++) {
+    this.dataStore[i] = Math.floor(Math.random() * (this.numElements + 1));
+  }
+}
+
+function clear() {
+  this.dataStore = [];
+}
+
+function insert(element) {
+  this.dataStore[this.pos++] = element;
+}
+
+function print() {
+  var output = '';
+  for (var i = 0; i < this.numElements; i++) {
+    output += this.dataStore[i] + ' ';
+    if (i > 0 && i % 10 === 0) {
+      output += '\n';
+    }
+  }
+  console.log(output);
+}
+
+function swap(arr, index1, index2) {
+  var temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
+
+function bubbleSort() {
+  var swapped = true;
+  var length = this.dataStore.length;
+  while (swapped) {
+    swapped = false;
+    for (var i = 0; i < length - 1; i++) {
+      if (this.dataStore[i] > this.dataStore[i + 1]) {
+        this.swap(this.dataStore, i, i + 1);
+        swapped = true;
+      }
+    }
+  }
+}
+
+function selectionSort() {
+  var length = this.dataStore.length;
+  for (var i = 0; i < length - 1; i++) {
+    for (var j = i + 1; j < length; j++) {
+      if (this.dataStore[j] < this.dataStore[i]) {
+        this.swap(this.dataStore, i, j);
+      }
+    }
+  }
+}
+
+function insertionSort() {
+  var length = this.dataStore.length;
+  for (var i = 1; i < length; i++) {
+    for (var j = i; j > 0 && this.dataStore[j - 1] > this.dataStore[j]; j--) { // work backwards from the outer loop index
+      swap(this.dataStore, j - 1, j);
+    }
   }
 }
 
--- TESTS/Sorting_test.spec.js
+++ TESTS/Sorting_test.spec.js
@@ -1,21 +1,35 @@
 var CArray = require('../Sorting.js');
 
 describe("Sorting", function() {
-  var arr = new CArray();
+  var numElements = 100;
+  var arr = new CArray(numElements);
 
   beforeEach(function() {
-    
+    arr.clear();
+    arr.setData();
   });
 
-  it("tests showGraph", function() { 
-    g.showGraph();
+  it("tests bubbleSort", function() {
+    console.log('Before bubbleSort()...'); 
+    arr.print();
+    arr.bubbleSort();
+    console.log('After bubbleSort()...'); 
+    arr.print();
   });
 
-  it("tests dfs", function() { 
-    g.dfs(0);  
+  it("tests selectionSort", function() { 
+    console.log('Before selectionSort()...'); 
+    arr.print();
+    arr.selectionSort();
+    console.log('After selectionSort()...'); 
+    arr.print();  
   });
 
-  it("tests bfs", function() {
-    g.bfs(0);
+  it("tests insertionSort", function() { 
+    console.log('Before insertionSort()...'); 
+    arr.print();
+    arr.insertionSort();
+    console.log('After insertionSort()...'); 
+    arr.print();  
   });
 });

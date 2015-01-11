c2ca1d7c9ba370fed1e5cc6bd00ef5b1db2e8344
fulltilt <dave.doria@gmail.com>
2015-01-10 22:00:17 -0800

    finished dfs

--- Graph2.js
+++ Graph2.js
@@ -1,14 +1,65 @@
-function IntegerPair() {
-
+function IntegerPair(i, w) {
+	this.id = i;
+	this.weight = w;
 }
 
 function Graph(v) {
-	var vertices = v;
-  var adjacencyList = [];
+	this.vertices = v;
+  this.adjacencyList = [];
 
-	var DFS_WHITE = -1; // normal DFS
-  var DFS_BLACK = 1;
-  var DFS_GRAY = 2;
+  // initialize adjacency list
+  for (var i = 0; i < this.vertices; i++) {
+  	this.adjacencyList[i] = [];
+  }
+
+  /*this.DFS_WHITE = -1; // normal DFS
+  this.DFS_BLACK = 1;
+  this.DFS_GRAY = 2;*/
+  this.UNVISITED = -1;
+  this.VISITED = 1;
+  this.visited = [];
+
+  // initialize visited list
+  for (i = 0; i < this.vertices; i++) {
+  	this.visited[i] = this.UNVISITED;
+  }
 }
+
+Graph.prototype = {
+	// NOTE: input graph must be undirected
+	dfs: function() {
+		var connectedComponents = 0;
+
+		for (var i = 0; i < this.vertices; i++) {
+			if (this.visited[i] === this.UNVISITED) {
+				console.log('Component ', ++connectedComponents, ', visit: ');
+				this.dfsHelper(i);
+			}
+		}
+
+		console.log('There are ', connectedComponents, ' connected components.')
+	},
+
+	dfsHelper: function(v) {
+		console.log(v);
+		var length = this.adjacencyList[v].length, i;
+		this.visited[v] = this.VISITED;
+
+		for (var i = 0; i < length; i++) {
+			var neighborVertex = this.adjacencyList[v][i].id;
+			
+			if (this.visited[neighborVertex] === this.UNVISITED) {
+				this.dfsHelper(neighborVertex);
+			}
+		}
+	}
+};
+
+var Graph = function() {
+  return {
+    Graph: Graph,
+    IntegerPair: IntegerPair
+  }
+}();
 
 module.exports = Graph;
--- TESTS/Graph2_test.spec.js
+++ TESTS/Graph2_test.spec.js
@@ -2,7 +2,7 @@
 var Graph = require('../Graph2.js');
 
 describe("Graph2", function() {
-  var g = new Graph();
+  var graph = new Graph.Graph();
 
   it('tests dfs', function() {
 /*
@@ -15,6 +15,21 @@ describe("Graph2", function() {
     2 7 0 8 0
     1 6 0
     1 6 0 */
-
+    var g = new Graph.Graph(9);
+    g.adjacencyList[0].push(new Graph.IntegerPair(1, 0));
+    g.adjacencyList[1].push(new Graph.IntegerPair(0, 0));
+    g.adjacencyList[1].push(new Graph.IntegerPair(2, 0));
+    g.adjacencyList[1].push(new Graph.IntegerPair(3, 0));
+    g.adjacencyList[2].push(new Graph.IntegerPair(1, 0));
+    g.adjacencyList[2].push(new Graph.IntegerPair(3, 0));
+    g.adjacencyList[3].push(new Graph.IntegerPair(1, 0));
+    g.adjacencyList[3].push(new Graph.IntegerPair(2, 0));
+    g.adjacencyList[3].push(new Graph.IntegerPair(4, 0));
+    g.adjacencyList[4].push(new Graph.IntegerPair(3, 0));
+    g.adjacencyList[6].push(new Graph.IntegerPair(7, 0));
+    g.adjacencyList[6].push(new Graph.IntegerPair(8, 0));
+    g.adjacencyList[7].push(new Graph.IntegerPair(6, 0));
+    g.adjacencyList[8].push(new Graph.IntegerPair(6, 0));
+    g.dfs();
   });
 });

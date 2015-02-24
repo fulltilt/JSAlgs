6ce3946ebfe12ca07ce3264c2321841827520875
Dave D <dave.doria@gmail.com>
2015-02-23 16:16:07 -0800

    update to KargerMinCut

--- Misc/KargerMinCut.js
+++ Misc/KargerMinCut.js
@@ -82,9 +82,68 @@ for (i = 0; i < len; i++) {
   input[i] = input[i].trim().split('\t');
   graph[i + 1] = input[i].slice(1);
 }
+//console.log(graph)
 
 //console.log(graph[200]);
 //console.log(graph[35]);
 // console.log(Object.keys(graph).length);
 // delete graph[3];
 // console.log(Object.keys(graph).length);
+
+// var graph = {};
+// graph[1] = [2, 3, 4, 7];
+// graph[2] = [1, 3, 4];
+// graph[3] = [1, 2, 4];
+// graph[4] = [1, 2, 3, 5];
+// graph[5] = [4, 6, 7, 8];
+// graph[6] = [5, 7, 8];
+// graph[7] = [1, 5, 6, 8];
+// graph[8] = [5, 6, 7];
+// console.log(graph)
+
+
+//Math.floor((Math.random() * len) + 1) // generate a random number between 1 and the length of the graph inclusive
+function getRandomEdge(graph) {
+  var arr = Object.keys(graph),
+      len = arr.length;
+
+  // keep generating random indices until you get two indices that are in each others adjacency list      
+  do {
+    x = Math.floor((Math.random() * len));
+    y = Math.floor((Math.random() * len));
+  } while (graph[parseInt(arr[x])].indexOf(parseInt(arr[y])) === -1 && (x === y));  // need the x === y check because as graph contracts adjacency lists will have multiple duplicates
+
+  return [parseInt(arr[x]), parseInt(arr[y])];
+}
+
+while (Object.keys(graph).length > 2) {
+  var remainingVertices = Object.keys(graph),
+      randomEdgeToDelete = getRandomEdge(graph);
+
+  // consolidate vertices to the 2nd vertex in edge
+  var dVertex = randomEdgeToDelete[0],        // disappearing vertex
+      cVertex = randomEdgeToDelete[1],        // consolidated vertex
+      dvList = graph[dVertex];                // list of adjacent nodes of disappearing Vertex
+
+//console.log(graph, 'dVertex:', dVertex, 'cVertex:', cVertex, 'dvList:', dvList);
+  // remove references between disappearing vertex and consolidated vertex
+  graph[cVertex].splice(graph[cVertex].indexOf(dVertex), 1);  // remove reference to disappearing vertex from consolidated vertex
+  graph[dVertex].splice(graph[dVertex].indexOf(cVertex), 1);  // remove reference to consolidated vertex from disappearing vertex
+
+  // iterate through the soon to be deleted vertex and change references in other vertices to the consolidated vertex
+  for (var i = 0; i < dvList.length; i++) {
+    try {
+      var index = graph[dvList[i]].indexOf(dVertex);
+      graph[dvList[i]][index] = cVertex;
+    } catch (err) {
+      console.log(err)
+      console.log(graph, 'dVertex:', dVertex, 'cVertex:', cVertex, 'dvList:', dvList, 'index:', index, 'i:', i);
+      throw new Error('');
+    }
+  }
+
+  graph[cVertex] = graph[cVertex].concat(graph[dVertex]);   // append disappearing vertex adjacency list to consolidated vertex adjacencylist
+  delete graph[dVertex];                            // delete disappearing vertex
+}
+
+console.log(graph);

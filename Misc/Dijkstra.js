f9c4e150b6226e8f79bd2797c8e3cadbee403401
fulltilt <dave.doria@gmail.com>
2015-03-03 20:38:48 -0800

    updates to Dijkstra.js

--- Misc/Dijkstra.js
+++ Misc/Dijkstra.js
@@ -8,12 +8,11 @@ var inputFile = process.argv[2],
     text = fs.readFileSync(inputFile, 'utf-8'),
     //input = text.split('\r\n'),
     input = text.split('\n'),
-    len = input.length, i;
+    vertices = input.length, i;
 
-//[ '1 2,5 3,1', '2 4,10 1,5', '3 4,1 1,1', '4 2,10 3,1' ]
-// note: the input reflects that this is an directed graph
+// note: the input reflects that this is an undirected graph
 var graph = [];
-for (i = 0; i < len; i++) {
+for (i = 0; i < vertices; i++) {
   //input[i] = input[i].trim().split('\t');
   input[i] = input[i].trim().split(' ');
   graph[i + 1] = {};
@@ -26,30 +25,38 @@ for (i = 0; i < len; i++) {
   	graph[i + 1][temp[j][0]] = temp[j][1];
   }
 }
-console.log(graph)
-var visited = {},
-		dist = [],
-		source = 1, i;
 
-dist[1] = 0;
-visited[source] = true;
+//[ '1 2,5 3,1', '2 4,10 1,5', '3 4,1 1,1', '4 2,10 3,1' ]
+// [ ,
+//   { '2': 5, '3': 1 },
+//   { '1': 5, '4': 10 },
+//   { '1': 1, '4': 1 },
+//   { '2': 10, '3': 1 } ]
+
+var done = {},
+		currentVertex = 1, i;
+
+done[1] = 0;
+//visited[source] = true;
 
 // initialize shortest distance for vertices 2 through 200 to 1000000
-for (i = 2; i <= len; i++) {
-	dist[i] = 1000000;
+// for (i = 2; i <= vertices; i++) {
+// 	done[i] = Infinity;
+// }
+
+var reachable = {};
+while (Object.keys(done).length < vertices) {
+	updateReachable(currentVertex);
+	//console.log(reachable)
+	var closestVertex = getClosestVertex(reachable);
+	// bug: closest vertex isn't always reachable from currentVertex; it can be reachable from previously visited vertex
+	done[closestVertex] = done[currentVertex] + graph[currentVertex][closestVertex]; 
+	//console.log(currentVertex)
+	currentVertex = closestVertex;
+console.log(reachable, closestVertex, done)
 }
+console.log(done)
 
-// var reachable = {};
-// do {
-// 	updateReachable(source);
-// 	var vertex = getClosestVertex(reachable);
-// 	console.log(vertex)
-// 	dist[vertex] = dist[source] + graph[source][vertex]; 
-// //console.log(reachable);
-// //console.log(getClosestVertex(reachable));
-// } while (Object.keys(reachable).length > 0);
-
-// console.log(dist)
 /* code used to see what nodes are reachable
 	while (Object.keys(reachable).length > 0) {
 	var neighbors = Object.keys(reachable); 
@@ -66,12 +73,12 @@ for (i = 2; i <= len; i++) {
 
 function updateReachable(vertex) {
 	for (var k in graph[vertex]) {
-		if (k in reachable) {
-			if (graph[vertex][k] < reachable[k]) {
+		if (!(k in done)) {
+			if (k in reachable) {	// handle case when vertex is reachable from a prior visited vertex but wasn't marked done
+				reachable[k] = (reachable[k] < graph[vertex][k]) ? reachable[k] : graph[vertex][k];
+			} else {
 				reachable[k] = graph[vertex][k];
 			}
-		} else {
-			reachable[k] = graph[vertex][k];
 		}
 	}
 }
@@ -82,12 +89,12 @@ function getClosestVertex(arr) {
  			len = Object.keys(arr).length;
 
  	for (var v in arr) {
- 		if (graph[arr[v]] && arr[v] < closestLength) {
+ 		if (graph[v] && arr[v] < closestLength) {
  			closestLength = arr[v];
  			closestVertex = v;
  		}
  	}
 
- 	delete graph[closestVertex];
+ 	delete reachable[closestVertex];
  	return closestVertex;
 }

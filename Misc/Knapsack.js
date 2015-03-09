6947caebd3fb4b42874711a9090d5f0d237fdda4
fulltilt <dave.doria@gmail.com>
2015-03-09 09:57:51 -0700

    majority of Knapsack done, just need to fix bug(s)

---
+++ Misc/Knapsack.js
@@ -0,0 +1,56 @@
+// 2462812
+// 3264673
+var fs = require('fs');
+
+var inputFile = process.argv[2],
+    text = fs.readFileSync(inputFile, 'utf-8'),
+    input = text.split('\n').map(function(x) { return x.trim(); }),
+    maxSize = parseInt(input[0].split(' ')[0]),
+    items = parseInt(input[0].split(' ')[1]), i;
+console.log(maxSize, items)
+input.shift();
+
+var values = [],
+	weights = [],
+	len = input.length, i;
+
+// dummy values so these arrays aren't zero-indexed
+values[0] = -1;
+weights[0] = -1;
+
+for (i = 0; i < len; ++i) {
+	var temp = input[i].split(' ');
+	values.push(parseInt(temp[0], 10));
+	weights.push(parseInt(temp[1], 10));
+}
+console.log(values)
+var hash = [];
+for (i = 0; i <= items; ++i) {
+	hash[i] = [];
+	hash[i][0] = 0;	// set first column to be all zeroes
+}
+
+// set first row to be all zeroes
+for (i = 0; i <= maxSize; ++i) {
+	hash[0][i] = 0;
+}
+
+function knapsack(hash, values, weights, itemIndex, size) {
+	if (itemIndex === -1) {
+		return 0;
+	}
+
+	// if (hash[itemIndex][size]) {
+	// 	return hash[itemIndex][size];
+	// }
+
+	if (size - weights[itemIndex] < 0) {
+		return hash[itemIndex][size] = 0;
+	} else {
+		return hash[itemIndex][size] = Math.max(knapsack(hash, values, weights, itemIndex - 1, size),
+												knapsack(hash, values, weights, itemIndex - 1, size - weights[itemIndex]) + values[itemIndex]);
+	}
+}
+
+// x: size, y: item
+console.log(knapsack(hash, values, weights, items, maxSize))

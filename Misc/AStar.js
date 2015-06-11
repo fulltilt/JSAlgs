a526f82171bb06cc8af9697cf3c17dd55325598d
fulltilt <dave.doria@gmail.com>
2015-06-10 21:40:13 -0700

    Added Dijkstras algorithm and printing functions for the path and parents

--- Misc/AStar.js
+++ Misc/AStar.js
@@ -21,7 +21,7 @@ function neighbors(nodes, node) {
 	return result;
 }
 
-
+// initial Graph function
 var Graph = function() {
 	this.edges = {};
 
@@ -151,12 +151,12 @@ var SquareGrid = function(width, height) {
 	this.draw = function() {
 		var wallObjArr = {};
 		for (var i = 0; i < this.walls.length; ++i) {
-			wallObjArr[g.walls[i]] = true;
+			wallObjArr[this.walls[i]] = true;
 		}
 
-		for (var y = 0; y < g.height; ++y) {
+		for (var y = 0; y < this.height; ++y) {
 			var row = '';
-			for (var x = 0; x < g.width; ++x) {
+			for (var x = 0; x < this.width; ++x) {
 				if (wallObjArr[[x,y]] === true) {
 					row += '##';
 				} else {
@@ -164,7 +164,8 @@ var SquareGrid = function(width, height) {
 				}
 			}
 			console.log(row);
-		}		
+		}
+		console.log('\n')
 	}
 
 	this.drawWithParents = function(start, goal) {
@@ -175,34 +176,35 @@ var SquareGrid = function(width, height) {
 
 		var parents = BFS3(this, start, goal);
 
-		for (var y = 0; y < g.height; ++y) {
+		for (var y = 0; y < this.height; ++y) {
 			var row = '';
-			for (var x = 0; x < g.width; ++x) {
+			for (var x = 0; x < this.width; ++x) {
  				if (x === goal[0] && y === goal[1]) {	// print goal
-					row += 'ZZ';
+					row += 'ZZZ';
 				} 
 				else if (wallObjArr[[x,y]] === true) {	// print wall
-					row += '##';
+					row += '###';
 				} else if (parents[[x,y]] === undefined) {	// print '.' to signify that this was untouched
 					row += '. ';
 				} else {
 					var parent = parents[[x,y]]
 
 					if (parent === null) {	// we reached the start node
-						row += 'AA';
+						row += 'AAA';
 					} else if (x === parent[0] - 1) {
-						row += '->'
+						row += '-> '
 					} else if (x === parent[0] + 1) {
-						row += '<-';
+						row += '<- ';
 					} else if (y === parent[1] - 1) {
-						row += 'v ';
+						row += ' v ';
 					} else if (y === parent[1] + 1) {
-						row += '^ ';
+						row += ' ^ ';
 					}
 				}
 			}
 			console.log(row);
-		}		
+		}
+		console.log('\n')
 	}	
 }
 
@@ -240,3 +242,291 @@ g.walls = [
 ];
 g.draw();
 g.drawWithParents([8,7], [17,2]);
+
+
+// Grid which uses Dijkstra's algorithm to account for weights
+function GridWithWeights(squareGrid) {
+	this.squareGrid = squareGrid;
+	this.weights = {};
+
+	// a - from; b - to
+	this.cost = function(a, b) {
+		// this implementation doesn't rely on 'from'. In this case, if destination is in weights, return b's cost
+		// else return 1
+		var to = '' + b[0] + ',' + b[1]; // step we have to do for JavaScript since Object keys are always strings
+		return this.weights[to] === undefined ? 1 : this.weights[to];
+	}
+
+	this.drawWeightedGrid = function() {
+		var xLength = this.squareGrid.width,
+			yLength = this.squareGrid.height;
+
+		for (var y = 0; y < yLength; ++y) {
+			var row = '';
+			for (var x = 0; x < xLength; ++x) {
+				var val = this.weights['' + x + ',' + y];
+				if (val !== Infinity) {
+					if (val < 10) {
+						row += val + '  ';
+					} else {
+						row += val + ' ';
+					}
+				} else {
+					row += '###';
+				}
+			}
+			console.log(row);
+		}
+		console.log('\n');
+	}
+
+	this.drawWeightedGridWithParents = function(start, goal) {
+		var wallObjArr = {};
+		for (var i = 0; i < this.squareGrid.walls.length; ++i) {
+			wallObjArr[this.squareGrid.walls[i]] = true;
+		}
+
+		var parents = Dijkstra(this, start, goal)[0];
+
+		for (var y = 0; y < this.squareGrid.height; ++y) {
+			var row = '';
+			for (var x = 0; x < this.squareGrid.width; ++x) {
+ 				if (x === goal[0] && y === goal[1]) {	// print goal
+					row += 'ZZZ';
+				} 
+				else if (wallObjArr[[x,y]] === true) {	// print wall
+					row += '###';
+				} else if (parents[[x,y]] === undefined) {	// print '.' to signify that this was untouched
+					row += '.  ';
+				} else {
+					var parent = parents[[x,y]]
+
+					if (parent === null) {	// we reached the start node
+						row += 'AAA';
+					} else if (x === parent[0] - 1) {
+						row += '-> '
+					} else if (x === parent[0] + 1) {
+						row += '<- ';
+					} else if (y === parent[1] - 1) {
+						row += ' v ';
+					} else if (y === parent[1] + 1) {
+						row += ' ^ ';
+					}
+				}
+			}
+			console.log(row);
+		}
+		console.log('\n')
+	}		
+
+	this.reconstructPath = function(start, goal) {
+		var current = goal;
+		var path = [current];		
+		var nodes = {};
+		nodes[current] = 1;
+
+		var cameFrom = Dijkstra(this, start, goal)[0];
+
+		while (current[0] !== start[0] && current[1] !== start[1]) {
+			current = cameFrom[current];
+			path.push(current);
+			nodes[current] = 1;
+		}
+
+		var xLength = this.squareGrid.width,
+			yLength = this.squareGrid.height;
+
+		for (var y = 0; y < yLength; ++y) {
+			var row = '';
+			for (var x = 0; x < xLength; ++x) {
+				if (x === start[0] && y === start[1]) {
+					row += 'SSS';
+				} else if (x === goal[0] && y === goal[1]) {
+					row += 'GGG';
+				} else {
+					var tmp = '' + x + ',' + y;
+					if (nodes[tmp] !== undefined) {
+						row += '@@@';
+					} else {
+						row += ' . ';
+					}
+				}
+			}
+			console.log(row);
+		}
+		console.log('\n');		
+	}
+}
+
+var PriorityQueue = function() {
+	this.elements = [];
+
+	this.empty = function() {
+		return this.elements.length === 0;
+	}
+
+	this.put = function(priority, item) {
+		this.elements.push([priority, item]);
+
+		var len = this.elements.length,
+			index = len - 1;
+
+		while (index > 0) {
+			var node = this.elements[index],
+				parentIndex = Math.floor((index - 1) / 2);
+			
+			if (node[0] < this.elements[parentIndex][0]) {	// if nodes priority is higher, swim up
+				var temp = this.elements[index];
+				this.elements[index] = this.elements[parentIndex];
+				this.elements[parentIndex] = temp;
+			} else {
+				break;
+			}
+
+			index = parentIndex;
+		}
+	}
+
+	this.get = function() {
+		if (this.empty()) {
+			console.log('Cannot pop from empty queue!');
+			return;
+		}
+
+		if (this.elements.length === 1) {
+			return this.elements.pop();
+		}
+
+		var returnVal = this.elements[0],
+			index = 0;
+
+		var lastElement = this.elements.pop();
+		this.elements[index] = lastElement;
+
+		var len = this.elements.length;
+
+		while (true) {
+			var parent = this.elements[index];
+			var leftChildIndex = index * 2 + 1,
+				rightChildIndex = index * 2 + 2,
+				leftChild = null, 
+				rightChild = null;
+
+			if (leftChildIndex < len) {
+				leftChild = this.elements[leftChildIndex];
+			}
+
+			if (rightChildIndex < len) {
+				rightChild = this.elements[rightChildIndex];
+			}
+
+			if (leftChild === null && rightChild === null) {
+				break;
+			} else if (rightChild === null) {
+				if (leftChild[0] < parent[0]) {
+					var temp = this.elements[leftChildIndex];
+					this.elements[leftChildIndex] = this.elements[index];
+					this.elements[index] = temp;
+
+					index = leftChildIndex;
+				} else {
+					break;
+				}
+			} else {
+				var minChildIndex = (leftChild[0] < rightChild[0]) ? leftChildIndex : rightChildIndex;
+				
+				if (this.elements[minChildIndex][0] < parent[0]) {
+					var temp = this.elements[minChildIndex];
+					this.elements[minChildIndex] = this.elements[index];
+					this.elements[index] = temp;
+
+					index = minChildIndex;
+				} else {
+					break;
+				}
+			}
+		}
+
+		return returnVal;		
+	}
+}
+ 
+// var pq = new PriorityQueue();
+// pq.put(6,2);
+// pq.put(3,2);
+// pq.put(4,2);
+// pq.put(1,2);
+// console.log(pq.elements, pq.elements.length)
+// console.log(pq.get())
+// console.log(pq.elements, pq.elements.length)
+
+var Dijkstra = function(graph, start, goal) {
+	var frontier = new PriorityQueue(),
+		cameFrom = {},
+		costSoFar = {};
+
+	frontier.put(0, start);
+	cameFrom[start] = null;
+	costSoFar[start] = 0;
+
+	while (!frontier.empty()) {
+		var current = frontier.get()[1];
+
+		if (current[0] === goal[0] && current[1] === goal[1]) {
+			break;
+		}
+
+		var neighbors = graph.squareGrid.neighbors(current),
+			len = neighbors.length;
+
+		for (var i = 0; i < len; ++i) {
+			var newCost = costSoFar[current] + graph.cost(current, neighbors[i]);
+
+			if ((cameFrom[neighbors[i]] === undefined) || newCost < costSoFar[neighbors[i]]) {
+				costSoFar[neighbors[i]] = newCost;
+				priority = newCost;
+				frontier.put(priority, neighbors[i]);
+				cameFrom[neighbors[i]] = current;
+			}
+		}	
+	}
+
+	return [cameFrom, costSoFar];
+}
+
+var reconstructPath = function(cameFrom, start, goal) {
+	var current = goal;
+	var path = [current];
+	while (current[0] !== start[0] && current[1] !== start[1]) {
+		current = cameFrom[current];
+		path.push(current);
+	}
+	path = path.reverse();
+	return path;
+}
+
+var g2 = new SquareGrid(10, 10);
+g2.walls = [[1,7],[1,8],[2,7],[2,8],[3,7],[3,8]];
+var gw = new GridWithWeights(g2);
+
+/* note: using strings as keys instead of tuples as Object keys in JavaScript are always strings.
+For future reference, if I were to try gw.weights = { [0,0]: 5 }, I would get an error. But if I were to try
+gw.weights[[0,0]] = 5, it would work. However, the array would be converted into a string: gw.weights = { '0,0': 5 }
+*/
+gw.weights = {
+	'0,0':5, '1,0':4, '2,0':5, '3,0':6, '4,0':7 , '5,0':8 , '6,0':9 , '7,0':10, '8,0':11, '9,0':12,
+	'0,1':4, '1,1':3, '2,1':4, '3,1':5, '4,1':10, '5,1':13, '6,1':10, '7,1':11, '8,1':12, '9,1':13,
+	'0,2':3, '1,2':2, '2,2':3, '3,2':4, '4,2':9 , '5,2':14, '6,2':15, '7,2':12, '8,2':13, '9,2':14,
+	'0,3':2, '1,3':1, '2,3':2, '3,3':3, '4,3':8 , '5,3':13, '6,3':18, '7,3':17, '8,3':14, '9,3':19,
+	'0,4':1, '1,4':0, '2,4':1, '3,4':6, '4,4':11, '5,4':16, '6,4':21, '7,4':20, '8,4':15, '9,4':16,
+	'0,5':2, '1,5':1, '2,5':2, '3,5':7, '4,5':12, '5,5':17, '6,5':22, '7,5':21, '8,5':16, '9,5':17,
+	'0,6':3, '1,6':2, '2,6':3, '3,6':4, '4,6':9 , '5,6':14, '6,6':19, '7,6':16, '8,6':17, '9,6':18,
+	'0,7':4, '1,7':Infinity, '2,7':Infinity, '3,7':Infinity, '4,7':14, '5,7':19, '6,7':18, '7,7':15, '8,7':16, '9,7':17,
+	'0,8':5, '1,8':Infinity, '2,8':Infinity, '3,8':Infinity, '4,8':15, '5,8':16, '6,8':13, '7,8':14, '8,8':15, '9,8':16,
+	'0,9':6, '1,9':7, '2,9':8, '3,9':9, '4,9':10, '5,9':11, '6,9':12, '7,9':13, '8,9':14, '9,9':15
+};
+gw.drawWeightedGrid();
+gw.drawWeightedGridWithParents([1,4],[7,8])
+var results = Dijkstra(gw, [1,4],[7,8]);
+var path = reconstructPath(results[0],[1,4],[7,8]);
+gw.reconstructPath([1,4],[7,8]);
